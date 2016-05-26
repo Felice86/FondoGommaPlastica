5a3c76ea8ca71ae3c40147f3ae948fe6d321d212
Felice86 <fefex86@gmail.com>
2016-05-26 15:04:26 +0200

    Prima formazione applicazione

---
+++ FondoGommaPlastica/FondoGommaPlastica/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FondoGommaPlastica
+//
+//  Created by Felice Vimercati on 26/05/16.
+//  Copyright © 2016 ElpoEdizioni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
