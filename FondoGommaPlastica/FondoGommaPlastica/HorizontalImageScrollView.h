371b040e328c2d97bb260ba04423dc49d82d43e8
Felice86 <fefex86@gmail.com>
2016-05-31 09:19:42 +0200

    Sistemazione file

---
+++ FondoGommaPlastica/FondoGommaPlastica/HorizontalImageScrollView.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalImageScrollView.h
+//  FondoGommaPlastica
+//
+//  Created by Felice on 29/05/16.
+//  Copyright © 2016 ElpoEdizioni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HorizontalImageScrollView : UIScrollView
+
+@end
---
+++ FondoGommaPlastica/FondoGommaPlastica/HorizontalImageScrollView.m
@@ -0,0 +1,18 @@
+//
+//  HorizontalImageScrollView.m
+//  FondoGommaPlastica
+//
+//  Created by Felice on 29/05/16.
+//  Copyright © 2016 ElpoEdizioni. All rights reserved.
+//
+
+#import "HorizontalImageScrollView.h"
+
+
+@interface HorizontalImageScrollView()
+@end
+
+@implementation HorizontalImageScrollView
+
+
+@end
