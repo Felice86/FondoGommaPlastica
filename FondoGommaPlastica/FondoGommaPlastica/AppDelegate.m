ada8f5eb097eb52574622fa175d95a021ea9a4c7
Felice86 <fefex86@gmail.com>
2016-06-18 15:09:18 +0200

    Salvataggio prima della modifica allo scroll alto

--- FondoGommaPlastica/FondoGommaPlastica/AppDelegate.m
+++ FondoGommaPlastica/FondoGommaPlastica/AppDelegate.m
@@ -21,6 +21,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [Configurations sharedConfiguration];
+    [NSThread sleepForTimeInterval:3];
     return YES;
 }
 
